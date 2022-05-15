af231d416602b8f0d8cc8b4ed1ec5f01d119c66f
aborashed94 <m.al.omar94@gmail.com>
2022-05-16 02:26:58 +0400

    Create main logic

--- LAN Chat App/Program.cs
+++ LAN Chat App/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+namespace LAN_Chat_App
+{
+    class Program
+    {
+        private const int port = 8888;
+        static void Main(string[] args)
+        {
+            string deviceIP = IPHelper.GetMyIPAddress();
+            ////List<string> ipAddressesOnNetwork = IPHelper.GetAllIPAddresses(deviceIP);
+            IPHelper.GetAllIPAddresses(deviceIP);
+            Server server = new();
+            server.Create(deviceIP,port).Start().StartListen();
+            Client client = new();
+            while (true)
+            {
+                Console.WriteLine("Enter receiver ip:");
+                if (Console.ReadLine() is string ip)
+                    client.ConnectToServer(ip, port);
+                if (client.IsConnected())
+                {
+                    Console.WriteLine("Enter you message:");
+                    if (Console.ReadLine() is string message)
+                    {
+                        client.SendMessage(message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Fialed connected...");
+                }
+            }
+        }
+    }
+}
